6eca6801a678c7e548344e610b4e659949a0ca2f
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-06-18 14:07:18 +0100

    Simplifying `create_server` to only take in socket address

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 use axum::routing::{get, IntoMakeService};
 use axum::Router;
 use hyper::server::conn::AddrIncoming;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::net::SocketAddr;
 
 pub type Server = axum::Server<AddrIncoming, IntoMakeService<Router>>;
 
-pub fn create_server(port: u16) -> Server {
-    let ip = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
-    let socket = SocketAddr::new(ip, port);
+pub fn create_server(address: SocketAddr) -> Server {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    axum::Server::bind(&socket).serve(app.into_make_service())
+    axum::Server::bind(&address).serve(app.into_make_service())
 }
