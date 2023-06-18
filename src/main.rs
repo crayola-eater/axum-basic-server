15f7903b71f03392aa1f1d777139a7eced14ec0e
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-06-18 12:23:04 +0100

    Initialising `main` function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use rust_axum_deployment::create_server;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let port = env::var("PORT")
+        .expect("PORT environment variable should be set.")
+        .parse::<u16>()
+        .expect("PORT should valid u16");
+
+    let server = create_server(port);
+
+    server.await.expect("Server should run");
 }
