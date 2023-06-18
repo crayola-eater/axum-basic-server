4326ccbe855f0858817964ab02fa14ae6488a80b
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-06-18 13:14:45 +0100

    Updating address to be `0.0.0.0` as per docs

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 pub type Server = axum::Server<AddrIncoming, IntoMakeService<Router>>;
 
 pub fn create_server(port: u16) -> Server {
-    let ip = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+    let ip = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
     let socket = SocketAddr::new(ip, port);
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
     axum::Server::bind(&socket).serve(app.into_make_service())
