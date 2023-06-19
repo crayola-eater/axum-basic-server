0e15816bd3ebbe3152beeb318a3979a8505ec04b
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-06-19 20:40:58 +0100

    Writing basic test for health route

---
+++ tests/health_router.rs
@@ -0,0 +1,14 @@
+use asserhttp::*;
+
+mod helpers;
+use helpers::setup;
+
+#[tokio::test]
+async fn health_router_responds_correctly() {
+  let address = setup().await;
+
+  reqwest::get(format!("http://{address}/api/health"))
+    .await
+    .expect_status_ok()
+    .expect_body_absent();
+}
