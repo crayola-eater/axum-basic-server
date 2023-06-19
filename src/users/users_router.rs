4c6ab927413714a956cf0aec427d01b045268117
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-06-19 20:53:29 +0100

    Implementing basic handler for creating user

--- src/users/users_router.rs
+++ src/users/users_router.rs
@@ -9,3 +9,22 @@ pub async fn get_all_users(State(state): State<SharedUsersState>) -> impl IntoRe
   let users = state.lock().await.try_get_all_users().await.unwrap();
   Json(users)
 }
+
+#[derive(Debug, Deserialize)]
+pub struct CreateUserRequestBody {
+  username: String,
+}
+
+pub async fn create_user(
+  State(state): State<SharedUsersState>,
+  Json(body): Json<CreateUserRequestBody>,
+) -> impl IntoResponse {
+  let created = state
+    .lock()
+    .await
+    .try_create_user(body.username)
+    .await
+    .unwrap();
+
+  Json(created)
+}
