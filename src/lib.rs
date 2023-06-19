7a1084e7fcf93fa84058469bb9f981074644dfed
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-06-19 20:45:37 +0100

    Adding router for users

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,11 @@ mod users;
 pub type Server = axum::Server<AddrIncoming, IntoMakeService<Router>>;
 
 pub async fn create_server(address: SocketAddr) -> Server {
-  let app = Router::new().route("/api/health", get(|| async { StatusCode::OK }));
+  let users_router = users::create_users_router().await;
+
+  let app = Router::new()
+    .route("/api/health", get(|| async { StatusCode::OK }))
+    .nest("/api/users", users_router);
 
   axum::Server::bind(&address).serve(app.into_make_service())
 }
