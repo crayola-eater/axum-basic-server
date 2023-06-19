05698a0629aaa3667aa08db06c1d0382d036fcc4
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-06-19 20:50:45 +0100

    Adding model function to create user

--- src/users/users_model.rs
+++ src/users/users_model.rs
@@ -18,6 +18,18 @@ impl UsersCollection {
   pub async fn new_shared() -> Arc<Mutex<Self>> {
     Arc::new(Mutex::new(UsersCollection::default()))
   }
+
+  pub async fn try_create_user(&mut self, username: String) -> Result<User, ()> {
+    let created = User {
+      id: self.user_id,
+      username,
+    };
+
+    self.user_id += 1;
+    self.users.push(created.clone());
+
+    Ok(created)
+  }
 }
 
 pub type SharedUsersState = Arc<Mutex<UsersCollection>>;
