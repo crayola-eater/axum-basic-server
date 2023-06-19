80d24ec33f77e01e2bb4288ad09ca68220d48888
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-06-19 20:55:42 +0100

    Adding basic test for creating one user

---
+++ tests/users_router.rs
@@ -0,0 +1,28 @@
+use asserhttp::*;
+use futures::future;
+use reqwest::Client;
+use serde_json::json;
+
+mod helpers;
+use helpers::setup;
+
+#[tokio::test]
+async fn create_one_user() {
+  let address = setup().await;
+  let username = "Abe";
+
+  Client::new()
+    .post(format!("http://{address}/api/users"))
+    .json(&json!({ "username": username }))
+    .send()
+    .await
+    .expect_status_ok()
+    .expect_content_type_json()
+    .expect_body_json_eq(json!({ "id": 0, "username": "Abe" }));
+
+  reqwest::get(format!("http://{address}/api/users"))
+    .await
+    .expect_status_ok()
+    .expect_content_type_json()
+    .expect_body_json_eq(json!([{ "id": 0, "username": "Abe" }]));
+}
