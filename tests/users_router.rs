5ccdab4025ef56a4fdf618923765c3c032b681ed
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-06-19 20:56:37 +0100

    Adding basic test for getting users when collection is empty

--- tests/users_router.rs
+++ tests/users_router.rs
@@ -26,3 +26,14 @@ async fn create_one_user() {
     .expect_content_type_json()
     .expect_body_json_eq(json!([{ "id": 0, "username": "Abe" }]));
 }
+
+#[tokio::test]
+async fn get_all_users_when_no_users() {
+  let address = setup().await;
+
+  reqwest::get(format!("http://{address}/api/users"))
+    .await
+    .expect_status_ok()
+    .expect_content_type_json()
+    .expect_body_json_eq(json!([]));
+}
