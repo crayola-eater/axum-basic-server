d4b467d62ef10ba2c7ddbe698d3be2099ba5fdf4
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-06-20 18:23:19 +0100

    Adding route handler for getting user by id

--- src/users/users_router.rs
+++ src/users/users_router.rs
@@ -1,4 +1,5 @@
-use axum::extract::State;
+use axum::extract::{Path, State};
+use axum::http::StatusCode;
 use axum::response::{IntoResponse, Json};
 use axum::routing::{get, post, Router};
 use serde::Deserialize;
@@ -29,11 +30,25 @@ pub async fn create_user(
   Json(created)
 }
 
+pub async fn get_user_by_id(
+  Path(id): Path<usize>,
+  State(state): State<SharedUsersState>,
+) -> impl IntoResponse {
+  let guard = state.lock().await;
+  let user = guard.try_get_user_by_id(id).await;
+
+  match user {
+    Ok(user) => Json(user).into_response(),
+    Err(_) => (StatusCode::NOT_FOUND, "user not found").into_response(),
+  }
+}
+
 pub async fn create_users_router() -> Router {
   let shared_state = UsersCollection::new_shared().await;
 
   Router::new()
     .route("/", get(get_all_users))
     .route("/", post(create_user))
+    .route("/:user_id", get(get_user_by_id))
     .with_state(shared_state)
 }
