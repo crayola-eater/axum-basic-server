80c255c954c37a4cd544367e82fbfa70a31516ea
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-06-20 06:29:40 +0100

    Updating health router to send non-empty response

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use axum::http::StatusCode;
 use axum::routing::{get, IntoMakeService, Router};
 use hyper::server::conn::AddrIncoming;
 use std::net::SocketAddr;
@@ -11,7 +10,7 @@ pub async fn create_server(address: SocketAddr) -> Server {
   let users_router = users::create_users_router().await;
 
   let app = Router::new()
-    .route("/api/health", get(|| async { StatusCode::OK }))
+    .route("/api/health", get(|| async { "OK" }))
     .nest("/api/users", users_router);
 
   axum::Server::bind(&address).serve(app.into_make_service())
